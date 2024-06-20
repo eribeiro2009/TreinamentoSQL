cdb65d12def7202a64418a1a292dd3e47dea836d
brokenghosssst <juliomanoelsantosbonfim@gmail.com>
2024-06-20 19:33:48 -0300

    Commit Aula 16

---
+++ aula16.sql
@@ -0,0 +1,113 @@
+SELECT ENAME, JOB, SAL, DEPTNO
+FROM EMP
+WHERE JOB IN( 'MANAGER', 'PRESIDENT' )
+UNION ALL
+SELECT ENAME, JOB, SAL, DEPTNO
+FROM EMP
+WHERE JOB NOT IN( 'MANAGER', 'PRESIDENT' )
+
+
+SELECT HIREDATE
+       , YEAR(HIREDATE) AS ANO
+       , MONTH(HIREDATE) AS MES
+	   , DAY(HIREDATE) AS DIA 
+	   , DATEPART(WEEK, HIREDATE) AS SEMANA 
+FROM EMP
+WHERE YEAR(HIREDATE) BETWEEN 1980 AND 1987
+ORDER BY YEAR(HIREDATE)
+
+SELECT DISTINCT JOB FROM EMP 
+
+FUNÇÕES DE GRUPO 
+COUNT(*)
+SUM()
+AVG() 
+MIN()
+MAX() 
+
+USE AdventureWorks
+SELECT Firstname
+       , LEFT(FirstName,3) as left_
+	   , RIGHT(FirstName,3) as right_
+	   , substring(FirstName, 3,4) as substr
+FROM Person.Person
+--WHERE LEFT(UPPER(FirstName),3) = 'DR'
+
+
+
+
+SELECT * 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE UPPER(COLUMN_NAME) LIKE '%COMMENT%'
+
+
+SELECT * 
+FROM Production.ProductReview
+WHERE UPPER(Comments) LIKE '%WOMAN%'
+
+|Maria <> MARIA <> maria <> mAria
+
+
+FUNÇÕES DE DATA
+DATEPART()
+YEAR()
+MONTH()
+DAY()
+
+FUNÇÕES DE STRING
+UPPER('abacaxi') = 'ABACAXI'- transforma em maisculo
+LOWER('Abacaxi') 'abacaxi'- minusculo
+LEFT(string,qtd) retorna da string a quantidade de caracteres a esquerda
+LEFT( 'ABACAXI',3) = 'ABA'
+RIGHT( 'ABACAXI',3) = 'AXI'
+SUBSTRING(string, inicio, fim)
+SUBSTRING('ABACAXI', 3,3) = 'ACA'
+CONCAT = concatena strings
+REPLACE( 'São Paulo/SP', '/', '-' ) =  'São Paulo-SP'
+
+select concat(cidade, '/', estado)
+
+USE INICIAL
+CREATE TABLE CIDADES(
+NOME_CIDADE VARCHAR(50),
+ESTADO VARCHAR(2)
+)
+SELECT CONCAT(nome_cidade, '/', estado) as cidade FROM CIDADES
+select NOME_CIDADE, replace(nome_cidade, '/','-') as cidade from CIDADES
+
+INSERT INTO CIDADES VALUES( 'São Paulo', 'SP' )
+INSERT INTO CIDADES VALUES( 'Carapicuiba', 'SP' )
+INSERT INTO CIDADES VALUES ('Manaus', 'AM' )
+
+
+DELETE FROM CIDADES WHERE NOME_CIDADE = 'São Paulo/SP'
+
+SELECT * FROM CIDADES
+
+-- COMO DESCOBRIR SE EXISTE REGISTRO DUPLICADO
+SELECT NOME_CIDADE, ESTADO, COUNT(*)
+FROM CIDADES 
+GROUP BY NOME_CIDADE, ESTADO
+HAVING COUNT(*) > 1
+
+-- RESOLVER A DUPLICIDADE **********
+DELETE CIDADES
+FROM ( SELECT NOME_CIDADE, ESTADO,NUMERO_DA_LINHA
+       FROM (  SELECT NOME_CIDADE, ESTADO, ROW_NUMBER() OVER(PARTITION BY NOME_CIDADE, ESTADO ORDER BY NOME_CIDADE, ESTADO) AS NUMERO_DA_LINHA
+               FROM CIDADES ) AS T  ) AS T
+WHERE NUMERO_DA_LINHA > 1
+
+
+
+Cidade, Estado
+São Paulo/SP
+Carapicuíba/SP
+Manaus/AM
+
+SELECT CONCAT(FirstName, ' ', MiddleName,' ', LastName) as FullName
+FROM Person.Person 
+
+
+
+SELECT * FROM EMP
+
