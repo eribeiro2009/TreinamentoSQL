8cd05f04ec6602903a1d50a661b60dafdd20ad8e
brokenghosssst <juliomanoelsantosbonfim@gmail.com>
2024-06-22 11:23:14 -0300

    Commit aula 17

--- Aula17.sql
+++ Aula17.sql
@@ -19,12 +19,12 @@ WHERE id = 34
 
 ALTER TABLE dbo.test ALTER COLUMN nome varchar(100)
 
-ALTER TABLE dbo.test ADD endereço VARCHAR(100)
+ALTER TABLE dbo.test ADD endereï¿½o VARCHAR(100)
 
-ALTER TABLE dbo.test ADD endereço VARCHAR(100) NOT NULL
+ALTER TABLE dbo.test ADD endereï¿½o VARCHAR(100) NOT NULL
 
 UPDATE dbo.test
-SET endereço = 'Endereco 2'
+SET endereï¿½o = 'Endereco 2'
 WHERE id = 33
 
 --DROP TABLE para elminar tabela
@@ -39,6 +39,57 @@ WHERE id = 33
 TRUNCATE TABLE dbo.test
 
 DROP TABLE dbo.test
+
+SELECT * FROM EMP
+
+SELECT *
+INTO EMP_CLONE 
+FROM EMP
+
+--pega o resultado da query e cria uma nova tabela com os mesmos registros da query
+
+SELECT * FROM EMP_CLONE
+
+--VIEW Ã© baseada no resultado de uma query
+
+CREATE VIEW vw_EMP as
+SELECT * FROM EMP
+
+SELECT *
+FROM dbo.vw_EMP
+WHERE DEPTNO = 30
+
+INSERT INTO EMP VALUES(7980, 'GUILHERME', 'SALESMAN', 7698, '2024-06-22', 1600.50, 500.00, 30)
+
+
+CREATE VIEW vw_EMPDEP as
+SELECT   d.DEPTNO as cod_dep
+		,d.DNAME as nome_dep
+		,sum(e.SAL) as soma_sal
+FROM EMP as e
+INNER JOIN DEPT as d
+ON e.DEPTNO = d.DEPTNO 
+GROUP BY d.DEPTNO, d.DNAME
+
+SELECT *
+FROM dbo.vw_EMPDEP
+WHERE cod_dep = 30
+
+CREATE VIEW vw_BLAKE as
+SELECT *
+FROM EMP 
+WHERE DEPTNO = 30
+
+CREATE VIEW vw_BLAKEDEP as
+SELECT   b.*
+		,d.DNAME
+FROM vw_BLAKE as b
+INNER JOIN DEPT as d
+ON b.DEPTNO = d.DEPTNO
+
+SELECT *
+FROM vw_BLAKEDEP
+
 
 
 
